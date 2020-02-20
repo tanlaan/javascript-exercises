5d37aa9a8c2f30fa1bfbae3b214ff32b8889e698
tanlaan <tanlaan33@gmail.com>
2020-02-20 13:06:12 -0800

    Just realized I should be putting this on github.

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
 const removeFromArray = function() {
-
+    let things, toRemove=[];
+    for (let i=0; i < arguments.length; i++){
+        if (i === 0){
+            things = arguments[i];
+        } else {
+            toRemove.push(arguments[i]);
+        }
+    }
+    for(let i=0; i < toRemove.length; i++){
+        for(let j=0; j < things.length; j++){
+            if(toRemove[i] === things[j]){
+                things.splice(j, 1);
+            }
+        }
+    }
+    return things;
 }
 
 module.exports = removeFromArray
--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeat) {
+    let phrase = ''
+    if (repeat < 0) phrase = 'ERROR';
+    for (let i = 0; i < repeat; i++){
+        phrase += word;
+    }
+    return phrase;
 }
 
 module.exports = repeatString

0d4f7e120210f7e4f2770ddb82c04e64b2cf5f4b
tanlaan <tanlaan33@gmail.com>
2020-02-21 09:01:48 -0800

    Finished sumAll problem.

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start, finish) {
+    let total = 0;
+    let first, last;
+    if (start < 0 || finish < 0 || typeof(start) !== 'number' || typeof(finish) !== 'number' ){
+        return 'ERROR';
+    } else if (start < finish){
+        first = start;
+        last = finish;
+    } else {
+        first = finish;
+        last = start;
+    }
+    for (let i=first;i <= last; i++){
+        total += i;
+    }
+    return total;
 }
 
 module.exports = sumAll

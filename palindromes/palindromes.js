298efe8899633524edefc52713d41f7189a45ca2
Chris Bolas <tanlaan33@gmail.com>
2020-10-14 22:58:12 -0700

    Finished the palindrom exercise.

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function() {
+const palindromes = palindrome => {
+    let prettyPalindrome = palindrome.replace(/[.,!?\\-]/g, "")
+    prettyPalindrome = prettyPalindrome.replace(/\s/g, "")
+    prettyPalindrome = prettyPalindrome.toLowerCase()
+    console.log(prettyPalindrome)
+    function palindromeCheck(word){
+        if(word.length <= 1){
+            return true
+        } else {
+            return word.endsWith(word.charAt(0)) && palindromeCheck(word.slice(1, word.length - 1))
+        }
+    }
+    return palindromeCheck(prettyPalindrome)
 
 }
 

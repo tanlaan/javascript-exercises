59d626ca53c5665f4e110bec693b09e905a3a1a7
Chris Bolas <tanlaan33@gmail.com>
2020-10-16 11:17:53 -0700

    Finished getTheTitles exercise

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(titles) {
+    // Took me a while to realize I wanted map no reduce
+    return titles.map(book => book['title'])
 }
 
 module.exports = getTheTitles;

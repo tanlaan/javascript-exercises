df69452c3092ddd9bb4395f7e5e44924c93b2716
Chris Bolas <tanlaan33@gmail.com>
2020-10-15 22:07:09 -0700

    Finished fibonacci exercise. Remembered the trick.

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(number < 1){
+        return 'OOPS'
+    } else if(number == 1) {
+        return 1
+    } else if(number == 2){
+        return 1
+    } else {
+        return fibonacci(number - 1) + fibonacci(number - 2)
+    }
 }
 
 module.exports = fibonacci
